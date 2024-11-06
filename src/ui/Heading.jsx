a2daf30cdea96eeaaf5e81fed699c388c396e20c
Mmy2000 <mm.yousef811@gmail.com>
2024-11-06 16:53:19 +0200

    make an filter by categroy or subcategory

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Heading({name} ) {
   return (
     <div className="flex w-full relative">
-      <h1 className="text-xl text-gray-900 dark:text-gray-200 w-full font-bold capitalize border-b border-b-gray-300 after:absolute after:left-0 after:bottom-0 after:w-[80px] after:h-[2px] after:bg-primary-700 pb-2">
+      <h1 className="text-xl text-gray-900 dark:text-gray-200 w-full font-bold capitalize border-b border-b-gray-300 pb-2 heading-underline">
         {name}
       </h1>
     </div>
