8ef94cf8d3d207115ea00d99ea8f10813a9b839f
Mmy2000 <mm.yousef811@gmail.com>
2024-11-28 21:22:03 +0200

    add not found page

--- src/ui/CurrencyFormat.jsx
+++ src/ui/CurrencyFormat.jsx
@@ -4,7 +4,7 @@ import { NumericFormat } from "react-number-format";
 export default function CurrencyFormat({ className, value }) {
   return (
     <NumericFormat
-      className={`tracking-wider font-normal dark:bg-transparent inline-flex max-w-[160px] outline-none ${className || ""}`}
+      className={`tracking-wider font-normal bg-transparent inline-flex max-w-[160px] outline-none ${className || ""}`}
       prefix="$"
       value={value}
       decimalScale={3}
