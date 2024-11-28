96b45bb626a97a4c82ac6f2b641c49c72b290f9a
Mmy2000 <mm.yousef811@gmail.com>
2024-11-28 21:50:08 +0200

    fix some issues

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -353,7 +353,7 @@ const Cart = () => {
         icon={
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="w-5 h-5 text-red-500"
+            className="w-5 h-5 mr-1 text-red-500"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
